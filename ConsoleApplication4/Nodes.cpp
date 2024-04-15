4fb9da45a41c093a2ee411b1af1f592db0dcf73c
Maxime66410 <max66410@hotmail.fr>
2024-04-15 12:39:24 +0200

    Add Comment into class

--- ConsoleApplication4/Nodes.cpp
+++ ConsoleApplication4/Nodes.cpp
@@ -1,39 +1,43 @@
 ﻿#include "Nodes.h"
 
-Nodes::Nodes() = default;
+Nodes::Nodes() = default; // Constructeur par défaut
 
+// Constructeur avec un paramètre
 Nodes::Nodes(char na)
 {
     setName(na);
 }
 
+// Constructeur avec deux paramètres
 Nodes::Nodes(char na, const std::vector<std::pair<Nodes*, int>>& n)
 {
     setName(na);
     neighbors_ = n;
 }
 
+// Constructeur avec trois paramètres
 Nodes::Nodes(char na, Nodes* n, int w)
 {
     setName(na);
     addNeighbor(n, w);
 }
 
-Nodes::~Nodes()
-{
-}
+Nodes::~Nodes() = default; // Destructeur
 
+// Retourne les voisins
 std::vector<std::pair<Nodes*, int>> Nodes::getNeighbors() const
 {
     return neighbors_;
 }
 
+// Ajoute un voisin
 void Nodes::addNeighbor(Nodes* n, int w)
 {
     neighbors_.emplace_back(n, w);
     n->neighbors_.emplace_back(this, w);
 }
 
+// Ajoute plusieurs voisins
 void Nodes::addNeighbor(std::vector<std::pair<Nodes*, int>> n)
 {
     for(std::pair<Nodes*, int> p : n)
@@ -43,6 +47,7 @@ void Nodes::addNeighbor(std::vector<std::pair<Nodes*, int>> n)
     }
 }
 
+// Supprime un voisin
 void Nodes::removeNeighbor(Nodes* n)
 {
     for(std::vector<std::pair<Nodes*, int>>::iterator it = neighbors_.begin(); it != neighbors_.end(); ++it)
@@ -55,11 +60,13 @@ void Nodes::removeNeighbor(Nodes* n)
     }
 }
 
+// Supprime tous les voisins
 void Nodes::removeAllNeighbors()
 {
     neighbors_.clear();
 }
 
+// Affiche les voisins
 void Nodes::showNeighbors()
 {
     std::cout << getName() << " : {" << std::endl;
@@ -70,11 +77,13 @@ void Nodes::showNeighbors()
     std::cout << "}" << std::endl;
 }
 
+// Retourne le nom
 char Nodes::getName() const
 {
     return name_;
 }
 
+// Modifie le nom
 void Nodes::setName(char name)
 {
     name_ = name;
--- ConsoleApplication4/Nodes.h
+++ ConsoleApplication4/Nodes.h
@@ -5,24 +5,24 @@
 class Nodes
 {
 public:
-    Nodes();
-    Nodes(char);
-    Nodes(char, const std::vector<std::pair<Nodes*, int>>&);
-    Nodes(char, Nodes*, int);
-    ~Nodes();
+    Nodes(); // Constructeur par défaut
+    Nodes(char); // Constructeur avec un paramètre
+    Nodes(char, const std::vector<std::pair<Nodes*, int>>&); // Constructeur avec deux paramètres
+    Nodes(char, Nodes*, int); // Constructeur avec trois paramètres
+    ~Nodes(); // Destructeur
     
-    std::vector<std::pair<Nodes*, int>> getNeighbors() const;
+    std::vector<std::pair<Nodes*, int>> getNeighbors() const; // Retourne les voisins
     
-    void addNeighbor(Nodes*, int);
-    void addNeighbor(std::vector<std::pair<Nodes*, int>>);
-    void removeNeighbor(Nodes*);
-    void removeAllNeighbors();
-    void showNeighbors();
+    void addNeighbor(Nodes*, int); // Ajoute un voisin
+    void addNeighbor(std::vector<std::pair<Nodes*, int>>); // Ajoute plusieurs voisins
+    void removeNeighbor(Nodes*); // Supprime un voisin
+    void removeAllNeighbors(); // Supprime tous les voisins
+    void showNeighbors(); // Affiche les voisins
 
-    char getName() const;
-    void setName(char);
+    char getName() const; // Retourne le nom
+    void setName(char); // Modifie le nom
 
 private:
-    char name_;
-    std::vector<std::pair<Nodes*, int>> neighbors_;
+    char name_; // Nom du noeud
+    std::vector<std::pair<Nodes*, int>> neighbors_; // Voisins du noeud
 };
